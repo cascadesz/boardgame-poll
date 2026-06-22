40ecf77c6061d4bba11a7c72272021087ac611a0
CascadΞ <84241271+cascadesz@users.noreply.github.com>
2026-06-22 10:08:01 +0000

    UX/UI

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const API_URL = 'https://script.google.com/macros/s/AKfycbzLgECYjG4Io_Q1RTgJQoUh6FMpj0r_yBshP7ldMlmzgChDgdoOv2pwdoBfQcsrppk9/exec';
-const VERSION = 'v1.0.2';
+const VERSION = 'v1.0.3';
 
 const voteForm = document.getElementById('voteForm');
 const messageEl = document.getElementById('message');
